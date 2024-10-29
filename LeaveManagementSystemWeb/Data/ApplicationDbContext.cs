554ac51f90f1f8011fe6d93e57ef752edfb629b0
OCEAN\seada <seada@ocean.com.au>
2024-10-29 15:23:56 +1100

    added leave type

--- LeaveManagementSystemWeb/Data/ApplicationDbContext.cs
+++ LeaveManagementSystemWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace LeaveManagementSystemWeb.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<LeaveType> LeaveTypes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ LeaveManagementSystemWeb/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystemWeb.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
